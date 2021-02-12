985fc6ccf7fe09d384dfe0ad9af9119b57233a2e
unonewcreative <unonewcreative@gmail.com>
2021-02-12 01:00:16 +0100

    Change script

--- Section_JavaScript/Loops/script.js
+++ Section_JavaScript/Loops/script.js
@@ -71,9 +71,9 @@
 
 // Guessing game
 let maximum = parseInt(prompt("Enter the maximum number!"));
+if (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
 const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum)
-
-// Testing git
 
 
