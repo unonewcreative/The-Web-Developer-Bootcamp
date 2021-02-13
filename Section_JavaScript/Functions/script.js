ed475baf35ea32019d354d94969aa7ced5a5e280
unonewcreative <unonewcreative@gmail.com>
2021-02-13 22:49:51 +0100

    Change code

---
+++ Section_JavaScript/Functions/script.js
@@ -0,0 +1,24 @@
+// function singSing() {
+//     console.log('Do')
+//     console.log('Re')
+//     console.log('Mi')
+// }
+
+// singSing()
+// singSing()
+// singSing()
+
+// Input
+// function greet(firstName) {
+//     console.log(`Hey there, ${firstName}!`)
+// }
+
+// // Rant exercise
+function rant(message) {
+    for (let i = 0; i < 3; i++) {
+        console.log(message.toUpperCase())
+    }
+}
+
+rant("test")
+
